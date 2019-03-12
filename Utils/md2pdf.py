d236fb6f68d51afa75f6832c891dc17ab4fe0472
Zephyr <ryanwang96@hotmail.com>
2019-03-12 18:20:03 +0800

    test

--- Utils/md2pdf.py
+++ Utils/md2pdf.py
@@ -6,8 +6,9 @@ import time
 
 def main(args):
     start_time = time.time()
-    os.system("pandoc {} -o {} --latex-engine=xelatex -V mainfont='WenQuanYi Zen Hei'".format(args.input_file,args.output_file))
-
+    # os.system("pandoc {} -o text.doc --latex-engine=xelatex -V mainfont='WenQuanYi Zen Hei'".format(args.input_file))
+    # os.system("pandoc text.docx-o {} '".format(args.output_file))
+    os.system("pandoc {} -o {} --latex-engine=xelatex --template='../Utils/pm-template.latex'".format(args.input_file,args.output_file))
     print("Transform Complete within {} sec.".format(int(time.time()-start_time)))
 def parse_arguments(argv):
     parser = argparse.ArgumentParser()
